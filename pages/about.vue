37d7d67a4a49f9b4810ceded9f45a51c295e5919
sircatalyst <temibami@gmail.com>
2020-01-07 16:02:32 +0100

    jokes app

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>About Jokes</h1>
+    <p>This is an app that displays corny jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About App",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny jokes" 
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Welcome to the best Corny Jokes on the Web</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Welcome to Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
